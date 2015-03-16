f309d197e457310d612b10deb6ac5d6aacc2e722
柴田　義也 <shibatayoshiya@FL1-133-202-111-237.tky.mesh.ad.jp>
2015-03-16 23:42:27 +0900

    3/16 シバタ : ハイスコアを表示するためのクラスを作成

---
+++ ScoreViewController.h
@@ -0,0 +1,13 @@
+//
+//  ScoreViewController.h
+//  DragonRequest
+//
+//  Created by 柴田　義也 on 2015/03/16.
+//  Copyright (c) 2015年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScoreViewController : UIViewController
+
+@end
